f36f95db4e08efb0571feb5a69b7edea7757bde6
Mohamed Hany <mohamedhany61@gmail.com>
2023-07-30 01:39:34 +0300

    add random colors feature
    add random colors button and implement generateRGB function in draw function

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ let mouseDown = false;
 let eraserSelected = false;
 let eraser = document.querySelector(".eraser img");
 let deleteBtn = document.querySelector(".buttons .delete");
+let randomColors = false;
+let randomColorsBtn = document.getElementById("randomColors");
 //  number of pixels
 numberOfDivsInput.addEventListener("change", () => {
   if (numberOfDivsInput.value >= 100) {
@@ -31,6 +33,9 @@ eraser.onclick = () => {
 function draw(box) {
   if (eraserSelected) {
     box.style.backgroundColor = "aliceBlue";
+  } else if (randomColors) {
+    let rgb = generateRGB();
+    box.style.backgroundColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
   } else box.style.backgroundColor = "black";
 }
 window.addEventListener("mousedown", () => {
@@ -60,4 +65,17 @@ function createBoard() {
   }
 }
 
+//generate random colors
+function generateRGB() {
+  let rgb = [];
+  for (let i = 0; i < 3; i++) rgb[i] = (Math.random() * 255).toFixed();
+  return rgb;
+}
+randomColorsBtn.addEventListener("click", () => {
+  randomColors = !randomColors;
+  if (randomColors) randomColorsBtn.style.borderColor = "cyan";
+  else randomColorsBtn.style.borderColor = "transparent";
+});
+
+// create initial board
 createBoard();
